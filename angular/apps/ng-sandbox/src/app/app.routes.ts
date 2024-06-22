224476f0d1d011f059ee5c0a7fc15e8067f02166
Alexander Pahn <alexander.pahn@outlook.com>
2024-06-22 11:11:25 +0200

    setup some initial infrastructure

--- angular/apps/ng-sandbox/src/app/app.routes.ts
+++ angular/apps/ng-sandbox/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'forms',
+    loadChildren: () => import('sandbox-forms').then(m => m.formsRoutes)
+  },
+
+  {
+    path: 'home',
+    component: DashboardComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
