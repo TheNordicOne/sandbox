44bdf85e480a5c4bd80f34f29a83c9d454e1f5b3
Alexander Pahn <alexander.pahn@outlook.com>
2025-02-02 11:04:57 +0100

    added cypress - Jest Tests are broken

---
+++ angular/projects/tic-tac-toe/src/app/components/game-over.component.cy.ts
@@ -0,0 +1,7 @@
+import { GameOverComponent } from './game-over.component';
+
+describe('game-over.component.cy.ts', () => {
+  it('should create the component', () => {
+    cy.mount(GameOverComponent);
+  });
+});
