95354a266ce5611b516b0e677a72d08bc33c57fc
Alexander Pahn <alexander.pahn@outlook.com>
2025-02-24 09:38:26 +0100

    added example of astro component

--- astro-starlight/astro.config.mjs
+++ astro-starlight/astro.config.mjs
@@ -14,10 +14,7 @@ export default defineConfig({
         sidebar: [
             {
                 label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
+                autogenerate: { directory: 'guides' },
             },
             {
                 label: 'Reference',
