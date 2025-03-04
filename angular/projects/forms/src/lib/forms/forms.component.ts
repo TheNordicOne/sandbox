ecdf5099608d692cca75d7c75ae16d374a4a58c9
Alexander Pahn <alexander.pahn@outlook.com>
2025-03-04 11:43:57 +0100

    loading state of reusable form part is aggregated via a service

--- angular/projects/forms/src/lib/forms/forms.component.ts
+++ angular/projects/forms/src/lib/forms/forms.component.ts
@@ -1,12 +1,13 @@
-import {Component, inject, ViewEncapsulation} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReusableFormPartsComponent} from '../reusable-form-parts/reusable-form-parts.component';
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
-import {DynamicFormComponent} from '../dynamic-form/dynamic-form.component';
-import {exampleForm} from '../dynamic-form/example-form';
+import {Component, inject, ViewEncapsulation} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {ReusableFormPartsComponent} from '../reusable-form-parts/reusable-form-parts.component'
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms'
+import {DynamicFormComponent} from '../dynamic-form/dynamic-form.component'
+import {exampleForm} from '../dynamic-form/example-form'
 import {
   ReusableFormPartWithLoadingStateComponent
-} from '../reusable-form-part-with-loading-state/reusable-form-part-with-loading-state.component';
+} from '../reusable-form-part-with-loading-state/reusable-form-part-with-loading-state.component'
+import {LoadingService} from '../reusable-form-part-with-loading-state/loading.service'
 
 @Component({
   selector: 'sbf-forms',
@@ -20,10 +21,14 @@ import {
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss',
   encapsulation: ViewEncapsulation.None,
+  providers: [LoadingService],
 })
 export class FormsComponent {
   private fb = inject(FormBuilder)
+  private loadingService = inject(LoadingService)
   form = this.fb.group({})
+
+  somethingIsLoading = this.loadingService.isLoading
 
   logToConsole() {
     console.debug(this.form.value)
---
+++ angular/projects/forms/src/lib/reusable-form-part-with-loading-state/loading.service.ts
@@ -0,0 +1,29 @@
+import {computed, Injectable, signal, Signal} from '@angular/core';
+
+@Injectable()
+export class LoadingService {
+  private registrations = signal<Map<string, Signal<boolean>>>(new Map());
+
+  private isLoadingInter = computed(() => {
+    const loadingSignals = [...this.registrations().values()];
+    return computed(() => loadingSignals.some((signal) => signal()));
+  });
+
+  isLoading = computed(() => this.isLoadingInter()());
+
+  registerLoadingState(loadingSignal: Signal<boolean>, key: string) {
+    this.registrations.update((map) => {
+      const newMap = new Map(map);
+      newMap.set(key, loadingSignal);
+      return newMap;
+    });
+  }
+
+  removeLoadingState(key: string) {
+    this.registrations.update((map) => {
+      const newMap = new Map(map);
+      newMap.delete(key);
+      return newMap;
+    });
+  }
+}
